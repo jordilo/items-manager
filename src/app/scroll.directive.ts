bfb868367fe6636fbf2d7d91348512b6cf763115
jordi lopez galera <jordi8284@gmail.com>
2020-05-24 07:55:49 +0200

    run scroll events on safari

--- src/app/scroll.directive.ts
+++ src/app/scroll.directive.ts
@@ -1,9 +1,11 @@
-import { Directive, HostBinding, HostListener, Input, Output, EventEmitter, ElementRef, AfterViewInit } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input, Output, EventEmitter, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Directive({
   selector: '[appScroll]'
 })
-export class ScrollDirective implements AfterViewInit {
+export class ScrollDirective implements AfterViewInit, OnDestroy {
 
   @Input() public threshold = 75;
   @Output() public scrollBottomReached = new EventEmitter<void>();
@@ -15,21 +17,37 @@ export class ScrollDirective implements AfterViewInit {
   private scrollPosition = 0;
   private readonly secureMargin = 50;
   private containerHeight: number;
+  private scroll$: Subscription;
+  private mousewheel$: Subscription;
 
   constructor(private element: ElementRef<HTMLElement>) { }
   public ngAfterViewInit() {
     this.containerHeight = this.element.nativeElement.offsetHeight;
+
+    this.scroll$ = fromEvent(this.element.nativeElement, 'scroll')
+      .pipe(map((event: UIEvent) => (event.target as Element).scrollTop))
+      .subscribe((scrollTopPosition) => this.onScrollDown(scrollTopPosition));
+
+    this.mousewheel$ = fromEvent(this.element.nativeElement, 'mousewheel')
+      .pipe(map((event: WheelEvent) => event.deltaY > 0))
+      .pipe(tap((d) => console.log('DOMMouseScroll', d)))
+      .subscribe((isDownDirection) => this.onmouseWheel(isDownDirection));
+  }
+
+  public ngOnDestroy() {
+    this.scroll$.unsubscribe();
+    this.mousewheel$.unsubscribe();
   }
 
   @HostListener('window:resize', [])
   public windowResize() {
     this.containerHeight = this.element.nativeElement.offsetHeight;
   }
-  @HostListener('scroll', ['$event.target.scrollTop'])
+
   public onScrollDown(scrollPosition: number) {
     this.scrollPosition = scrollPosition;
   }
-  @HostListener('mousewheel', ['$event.deltaY > 0'])
+
   public onmouseWheel(goToBottom: boolean) {
     if (goToBottom && this.scrollPosition > this.containerHeight * (this.threshold / 100)) {
       this.scrollBottomReached.emit();
